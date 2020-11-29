5c67e2385ed7f0ecca6358f3788d18ff51049e09
xiangshowei <xiangshowei@gmail.com>
2020-11-28 23:46:42 -0600

    corrected postal rate calculation

--- COMP131-IntroductionToComputerScienceI/COMP131Exams/src/postalRateCalculator/RateCalculator.java
+++ COMP131-IntroductionToComputerScienceI/COMP131Exams/src/postalRateCalculator/RateCalculator.java
@@ -93,28 +93,27 @@ public class RateCalculator {
      */
     public int getPostageBill() {
         int standardWeight = 16;
-        double firstClassBaseRate = 0.34;
-        double secondClassClassBaseRate = 0.2;
-        double overWeightRate = 0.15;
+        int firstClassBaseRate = 34;
+        int secondClassClassBaseRate = 20;
+        int overWeightRate = 15;
         int cost = 0;
-
 
         if (rateClass == 1) {
             if (packageWeight <= standardWeight) {
-                cost = firstClassBaseRate * packageWeight;
+                cost = (firstClassBaseRate * packageWeight) / 100;
             }
         
             else {
-                cost = (firstClassBaseRate * standardWeight) + ((packageWeight - standardWeight) * overWeightRate);
+                cost = ((firstClassBaseRate * standardWeight) + ((packageWeight - standardWeight) * overWeightRate)) / 100;
             } 
         }
         else { // second class
             if (packageWeight <= standardWeight) {
-                cost = secondClassClassBaseRate * packageWeight;
+                cost = (secondClassClassBaseRate * packageWeight) / 100;
             }
         
             else {  
-                cost = (secondClassClassBaseRate * standardWeight) + ((packageWeight - standardWeight) * overWeightRate);
+                cost = ((secondClassClassBaseRate * standardWeight) + ((packageWeight - standardWeight) * overWeightRate)) / 100;
             }
         }
         
